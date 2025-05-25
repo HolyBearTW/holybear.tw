0051e18f9860d291684bfb2937aaad0663f04ec9
HolyBearTW <50009618+HolyBearTW@users.noreply.github.com>
2025-05-25 19:49:47 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,15 +1,15 @@
 // docs/.vitepress/theme/index.ts
 import MyCustomLayout from './MyCustomLayout.vue'
-import './style.css' // 載入你的樣式
+import './style.css'
+
+// 引入我們新的 MyVPDoc 組件
+import MyVPDoc from './components/MyVPDoc.vue'
 
 export default {
-  // 直接匯出 Layout 元件，而不是使用 extends
-  // MyCustomLayout.vue 內部會處理如何使用 DefaultTheme.Layout
   Layout: MyCustomLayout,
-
-  // 如果您有其他的 VitePress 主題相關的配置，可以在這裡添加
-  // 例如 enhanceApp (用於註冊全局組件或插件)
-  // enhanceApp({ app, router, siteData }) {
-  //   // app.component('MyGlobalComponent', MyGlobalComponent)
-  // }
+  // 使用 enhanceApp 函數註冊組件覆蓋
+  enhanceApp({ app }) {
+    // 告訴 VitePress，當需要渲染 VPDoc 時，使用我們的 MyVPDoc.vue
+    app.component('VPDoc', MyVPDoc);
+  }
 }
