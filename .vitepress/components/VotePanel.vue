d4b302e84c42727f56b35c15e775e4650d8eca22
HolyBearTW <50009618+HolyBearTW@users.noreply.github.com>
2025-05-25 22:55:48 +0800

    Create VotePanel.vue

---
+++ .vitepress/components/VotePanel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="vote('up')" :disabled="loading">👍 推 ({{ up }})</button>
+    <button @click="vote('down')" :disabled="loading">👎 噓 ({{ down }})</button>
+  </div>
+</template>
+<script setup>
+import { useVote } from './useVote'
+import { useData } from 'vitepress'
+import { computed } from 'vue'
+
+const { page } = useData()
+const articleId = computed(() => page.value.relativePath)
+const { up, down, vote, loading } = useVote(articleId.value)
+</script>
