b2049116cf2c5091f02d574ef13b19b557c45b71
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-05-26 23:01:39 +0800

    Update FbComments.vue

--- .vitepress/components/FbComments.vue
+++ .vitepress/components/FbComments.vue
@@ -4,6 +4,7 @@
     <div class="fb-comments" :data-href="pageUrl" data-width="100%" data-numposts="10"></div>
   </div>
 </template>
+
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vitepress'
@@ -15,7 +16,7 @@ const pageUrl = `https://holybear.me${route.path}`
 onMounted(() => {
   hydrated.value = true
   if (window.FB) {
-    window.FB.XFBML.parse();
+    window.FB.XFBML.parse()
   }
 })
 </script>
