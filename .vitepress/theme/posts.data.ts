938b8b2505dd98c61b643bdce475eab3ce4d2d3e
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-05-29 00:50:40 +0800

    Update posts.data.ts

--- .vitepress/theme/posts.data.ts
+++ .vitepress/theme/posts.data.ts
@@ -19,7 +19,7 @@ function getFsCreatedDate(relativePath: string): string {
   }
 }
 
-export default createContentLoader('blog/**/*.md', {
+export default createContentLoader('docs/blog/**/*.md', {
   excerpt: true,
   transform(raw) {
     const posts = raw
