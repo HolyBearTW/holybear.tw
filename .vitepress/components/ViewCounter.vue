f398af34cedc783dab3e6307664c55efcdde45b7
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-06-07 02:15:28 +0800

    Create ViewCounter.vue

---
+++ .vitepress/components/ViewCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="view-count">
+    | 瀏覽次數：{{ views === null ? "載入中..." : views }}
+  </span>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { incrementAndGetViews } from '../firebase/view-count'
+const props = defineProps({ slug: String })
+const views = ref(null)
+onMounted(async () => {
+  if (props.slug) {
+    views.value = await incrementAndGetViews(props.slug)
+  }
+})
+</script>
