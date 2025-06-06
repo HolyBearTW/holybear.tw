0117d25e933c1ab1753b5a54464409c95580a2da
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-06-07 02:27:03 +0800

    Update ViewCounter.vue

--- .vitepress/components/ViewCounter.vue
+++ .vitepress/components/ViewCounter.vue
@@ -10,8 +10,18 @@ import { incrementAndGetViews } from './view-count'
 const props = defineProps({ slug: String })
 const views = ref(null)
 onMounted(async () => {
+  console.log('[ViewCounter] slug:', props.slug)
   if (props.slug) {
-    views.value = await incrementAndGetViews(props.slug)
+    try {
+      views.value = await incrementAndGetViews(props.slug)
+      console.log('[ViewCounter] views:', views.value)
+    } catch (e) {
+      views.value = '錯誤'
+      console.error('[ViewCounter] 讀取失敗:', e)
+    }
+  } else {
+    views.value = '無slug'
+    console.warn('[ViewCounter] slug 為空')
   }
 })
 </script>
