3448a45b2c9e4262984b609e9dd8564aaf3e5042
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-06-20 01:22:55 +0800

    Update index.ts

--- .vitepress/locales/index.ts
+++ .vitepress/locales/index.ts
@@ -3,18 +3,22 @@ import zh_TW from './zh_TW'
 import en from './en'
 
 export default defineConfig({
-    locales: {
-        root: {
-            label: '繁體中文',
-            lang: zh_TW.lang,
-            themeConfig: zh_TW.themeConfig,
-            description: zh_TW.description
-        },
-        en: {
-            label: 'English',
-            lang: en.lang,
-            themeConfig: en.themeConfig,
-            description: en.description
-        }
+  locales: {
+    root: {
+      label: '繁體中文',
+      lang: zh_TW.lang,
+      title: '聖小熊的秘密基地',
+      description: zh_TW.description,
+      themeConfig: zh_TW.themeConfig,
+      base: '/'
+    },
+    en: {
+      label: 'English',
+      lang: en.lang,
+      title: "HolyBear's Secret Base",
+      description: en.description,
+      themeConfig: en.themeConfig,
+      base: '/en/'
     }
+  }
 })
