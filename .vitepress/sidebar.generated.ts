514b3b54b99b0f4894a5862f8ad138136613d5e7
System Administrator <github-actions[bot]@users.noreply.github.com>
2025-06-27 09:28:58 +0000

    chore: sync delete en/blog files

--- .vitepress/sidebar.generated.ts
+++ .vitepress/sidebar.generated.ts
@@ -3,10 +3,6 @@ export default [
   {
     "items": [
       {
-        "text": "測試上傳影片 & GitHub Actions",
-        "link": "/blog/test-250627-2"
-      },
-      {
         "text": "鋼索上的日子：一個年輕人的自由渴望",
         "link": "/blog/2025-06-13"
       },
