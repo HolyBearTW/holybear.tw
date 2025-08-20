94cb03e1113a4143b09cf82056f9501144d44394
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-08-20 17:25:46 +0800

    更新 MyCustomLayout.vue

--- .vitepress/theme/MyCustomLayout.vue
+++ .vitepress/theme/MyCustomLayout.vue
@@ -89,7 +89,7 @@
                muted
                @ended="hideIntro"
                @error="hideIntro">
-            <source src="https://tw.hicdn.beanfun.com/beanfun/GamaWWW/MapleStory/official/2020/videos/mKv.mp4?133956072189787870" type="video/mp4">
+            <source src="video/maple.mp4" type="video/mp4">
         </video>
         <button @click="hideIntro" class="skip-btn">Skip</button>
     </div>
