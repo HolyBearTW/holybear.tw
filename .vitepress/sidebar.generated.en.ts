844492a6fc7113b27187bf74693ea190cf13c0de
聖小熊 <50009618+HolyBearTW@users.noreply.github.com>
2025-08-24 00:35:03 +0800

    chore: 更新所有產物，確保 authors.json、sitemap.xml、sidebar 等正確排除 blog_list

--- .vitepress/sidebar.generated.en.ts
+++ .vitepress/sidebar.generated.en.ts
@@ -3,10 +3,6 @@ export default [
   {
     "items": [
       {
-        "text": "Blog Posts",
-        "link": "/en/blog/blog_list"
-      },
-      {
         "text": "Life on a Tightrope: A Young Person's Yearning for Freedom",
         "link": "/blog/2025-06-13"
       }
